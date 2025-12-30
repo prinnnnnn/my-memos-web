48aa22aa9dae5661618114e943e054722fe4fd46
prinnnnnn <prin.parinthorn@gmail.com>
2025-12-30 21:17:14 +0700

    /memos backbone

--- app/memos/page.tsx
+++ app/memos/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { gradientsStyles } from '@/lib/theme'
 
 const MemosPage = () => {
   return (
-    <div className="bg-white w-full">
-      <h1>Memos</h1>
+    <div className="w-full mx-auto px-4 py-8 bg-white">
+      <div>
+        <h1 className={`text-4xl font-bold mb-8 w-min ${gradientsStyles.text}`}>Memos</h1>
+      </div>
     </div>
   )
 }
--- lib/theme/index.ts
+++ lib/theme/index.ts
@@ -16,7 +16,3 @@ export const gradientsStyles = {
   text: `bg-linear-to-r from-[#3a86ff] via-[#9d4edd] to-[#c9184a] text-transparent bg-clip-text`,
   hovered: `hover:bg-linear-to-r hover:from-[#03045e] hover:via-[#5a189a] hover:to-[#a4133c]`,
 };
-
-// export const gradStyle = `bg-linear-to-r from-[#3a86ff] via-[#9d4edd] to-[#c9184a]`;
-// export const gradStyleText = `bg-linear-to-r from-[#3a86ff] via-[#9d4edd] to-[#c9184a] text-transparent bg-clip-text`;
-// export const gradStyleHovered = `hover:bg-linear-to-r hover:from-[#03045e] hover:via-[#5a189a] hover:to-[#a4133c]`;
